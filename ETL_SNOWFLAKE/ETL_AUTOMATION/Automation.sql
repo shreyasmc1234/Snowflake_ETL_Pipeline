e43a4bac1d41f742d6ed8f7409cf5751aa22417e
SHREYAS M C <140893678+shreyasmc1234@users.noreply.github.com>
2025-11-23 11:21:12 +0530

    Add ETL automation SQL for Snowflake setup

---
+++ ETL_SNOWFLAKE/ETL_AUTOMATION/Automation.sql
@@ -0,0 +1,62 @@
+------------------- Warehouse Creation --------------------------
+
+CREATE OR REPLACE WAREHOUSE etl_wh
+  WAREHOUSE_SIZE = 'SMALL'
+  AUTO_SUSPEND = 60
+  AUTO_RESUME = TRUE
+  INITIALLY_SUSPENDED = TRUE;
+
+
+-------------- Storage Integration ------------------------------
+
+
+CREATE OR REPLACE STORAGE INTEGRATION my_etl_integration
+TYPE=EXTERNAL_STAGE
+ENABLED=TRUE
+STORAGE_PROVIDER='S3'
+STORAGE_ALLOWED_LOCATIONS=('s3://your-bucket/path/')
+STORAGE_AWS_ROLE_ARN='arn:aws:iam::<your-account-id>:role/<your-s3-role>';
+
+CREATE OR REPLACE STAGE external_stage_s3_bronze
+file_format=(TYPE=CSV SKIP_HEADER=1 FIELD_OPTIONALLY_ENCLOSED_BY='"')
+pattern='.*csv.*'
+storage_integration=my_etl_integration
+url='s3://your-bucket/path/';
+
+------------- Pipe to automatically Ingest the data --------------
+
+CREATE OR REPLACE PIPE s3_to_bronze
+AUTO_INGEST=TRUE
+AS
+COPY INTO etl_snowflake.etl_bronze.employee_bronze
+FROM @external_stage_s3_bronze;
+
+--------- Task will trigger if there is a new/changed data in bronze--------------------------
+
+create or replace task bronze_to_silver
+warehouse='etl_wh'
+-- schedule='USING CRON 0 */10 * * * Asia/Kolkata'
+when system$stream_has_data('etl_snowflake.etl_bronze.stream1')
+as
+CALL etl_snowflake.etl_silver.employee_silver_history();
+
+------------Below task will trigger after the first task ------------------------------------
+
+create or replace task silver_to_gold
+warehouse='etl_wh'
+after bronze_to_silver
+as
+CALL  etl_snowflake.etl_gold.employee_gold_load();
+
+
+-----------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
